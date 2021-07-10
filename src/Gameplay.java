d163277bf1095ab93f8c063808ddc800a2c8f5b2
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-10 14:04:53 +0300

    set up of the classes

---
+++ src/Gameplay.java
@@ -0,0 +1,29 @@
+import java.awt.event.*;
+import javax.swing.JPanel;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+}
